d0bd28df9967bf61ad80fe4565ac2586708d13b7
Jacinto Palma <jxcinto12@gmail.com>
2020-03-04 20:04:16 -0500

    Gentrificacion1

---
+++ Gentrificacion_PalmaJ_ZambranoJ/WebServer/API_Prueba/api-web/src/app.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get , Response} from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller('/api')
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get('2DMap')
+  getMap1(@Response() res){
+    return res.render('2DMap')
+  }
+
+}
