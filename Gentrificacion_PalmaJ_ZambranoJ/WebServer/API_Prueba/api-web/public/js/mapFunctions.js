6548bba05080449fbd8cac355aa2a164ef522ad6
Jacinto Palma <jxcinto12@gmail.com>
2020-05-12 23:46:44 -0500

    Capa Análisis 2010
    Se cambia el script de cambio entre capas.
    Se añade leyenda y colores a la capa base.
    Se añade la capa de Análisis del 2010.
    Se añade popup a la capa de  Análisis del 2010.

--- Gentrificacion_PalmaJ_ZambranoJ/WebServer/API_Prueba/api-web/public/js/mapFunctions.js
+++ Gentrificacion_PalmaJ_ZambranoJ/WebServer/API_Prueba/api-web/public/js/mapFunctions.js
@@ -1,106 +1,33 @@
-
 // ******* MAPA DESPLEGADO AL INICIO **********
 var mapa;
+var vista;
+// Varaibles para Capas
 var capaInicial;
+var capaBarrios;
+var capaBarriosSector;
+var capaResultados2010;
+//Variables de features
 var FeatureLayerRico;
 var renderColor;
+
+//PopUps
 var popUpNombres;
+var popUp2;
 require([
     "esri/Map",
     "esri/views/MapView",
-    "esri/layers/FeatureLayer"
-], function(Map, MapView,FeatureLayer) {
-    FeatureLayerRico = FeatureLayer
+    "esri/layers/FeatureLayer",
+    "esri/widgets/Legend",
+    "esri/views/ui/DefaultUI"
+], function(Map, MapView,FeatureLayer,Legend) {
+    FeatureLayerRico = FeatureLayer;
+    Leyenda = Legend;
     //Llamada al mapa base
     var map = new Map({
         basemap: "streets"
     });
 
-    // ***** AÑADE COLOR A LA CAPA INICIAL *********
-    renderColor= {
-        type: "unique-value",
-        field: "FID",
-        defaultSymbol: { type: "simple-fill"},
-        uniqueValueInfos: [{
-            value: "1",
-            symbol: {
-                type: "simple-fill",
-                color: "#A3BCCC"
-            }//symbol
-        }, {
-            value: "2",
-            symbol: {
-                type: "simple-fill",
-                color: "#A3BDB3"
-            }
-        },{
-            value: "3",
-            symbol: {
-                type: "simple-fill",
-                color: "#A4BD99"
-            }//symbol
-        },{
-            value: "4",
-            symbol: {
-                type: "simple-fill",
-                color: "#A4BE7F"
-            }//symbol
-        },{
-            value: "5",
-            symbol: {
-                type: "simple-fill",
-                color: "#A4BE66"
-            }//symbol
-        },{
-            value: "6",
-            symbol: {
-                type: "simple-fill",
-                color: "#A4BE4D"
-            }//symbol
-        },{
-            value: "7",
-            symbol: {
-                type: "simple-fill",
-                color: "#A4BF33"
-            }//symbol
-        },{
-            value: "8",
-            symbol: {
-                type: "simple-fill",
-                color: "#A4BE1A"
-            }//symbol
-        },{
-            value: "9",
-            symbol: {
-                type: "simple-fill",
-                color: "#A4BF00"
-            }//symbol
-        },{
-            value: "10",
-            symbol: {
-                type: "simple-fill",
-                color: "#B9CF00"
-            }//symbol
-        },{
-            value: "11",
-            symbol: {
-                type: "simple-fill",
-                color: "#CEDF00"
-            }//symbol
-        },{
-            value: "12",
-            symbol: {
-                type: "simple-fill",
-                color: "#E2EF00"
-            }//symbol
-        },{
-            value: "13",
-            symbol: {
-                type: "simple-fill",
-                color: "#F7FF00"
-            }//symbol
-        },]
-    };
+
 
 
     //***** POPUP DE NOMBRES DE LA CAPA ********
@@ -113,8 +40,7 @@ require([
 
     capaInicial = new FeatureLayer({
 
-            url : "https://services6.arcgis.com/a7URR860GLlM8vfi/arcgis/rest/services/Barrios_CentroHistorico_F/FeatureServer",
-            renderer: renderColor,
+            url : "https://services9.arcgis.com/1dyQOpYtlvpIzdDa/arcgis/rest/services/Barrios_CentroHistorico_F/FeatureServer",
             outFields: ["FID","NOMBRE"],
             popupTemplate: popUpNombres,
             opacity: 0.7
@@ -132,38 +58,76 @@ require([
         zoom: 15
     });
     mapa = map;
+    vista = view;
 });
 
-
-
-
 //******** LLAMADA A NUEVAS CAPAS ***********
-var capaBarrios;
-var capaBarriosManzanas;
-function ocultar() {
-    mapa.remove(capaBarrios)
-}
+function cambioCapa(arreglo) {
+    console.log("recibi mi arreglo "+ arreglo);
+    if(arreglo=="Barrios - Sector"){
 
-function mostrar() {
-    capaBarrios = new FeatureLayerRico({
+        capaBarrios = new FeatureLayerRico({
+            url : "https://services9.arcgis.com/1dyQOpYtlvpIzdDa/arcgis/rest/services/Barrios_CentroHistorico_F/FeatureServer"
+        });
 
-        url : "https://services6.arcgis.com/a7URR860GLlM8vfi/arcgis/rest/services/Barrios_CentroHistorico_F/FeatureServer"
-    });
-    mapa.add(capaBarrios)
+        var legend = new Leyenda({
+            view: vista,
+            layerInfos: [
+                {
+                    layer: capaBarrios,
+                    title: "Barrios - Centro Histórico"
+                }
+            ]
+        });
 
-    capaBarriosManzanas = new FeatureLayerRico({
+        capaBarriosSector = new FeatureLayerRico({
+            url : "https://services9.arcgis.com/1dyQOpYtlvpIzdDa/arcgis/rest/services/barrios_sector/FeatureServer",
+            opacity: 0.1
+        });
 
-        url : "https://services6.arcgis.com/a7URR860GLlM8vfi/arcgis/rest/services/Barrios_Manzanas_F/FeatureServer"
-    });
-    mapa.add(capaBarriosManzanas)
-}
+        mapa.remove(capaInicial);
+        mapa.add(capaBarrios);
+        mapa.add(capaBarriosSector);
+        vista.ui.add(legend,"bottom-right")
 
-function cambioValor(evento){
-    console.log('evento', evento);
-    const ocultarSeleccionado = evento.target.value === "2";
-    if(ocultarSeleccionado){
-        ocultar()
-    }else{
-        mostrar()
+
+
+
+    }else if(arreglo == "Resultados Análisis 2010"){
+
+        popUp2={
+            "title": "INFORMACIÓN DEL SECTOR",
+            "content": function () {
+                return "Total Hogares: {T_HO_S}" +
+                    "<br> Total Personas: {T_PE_S}" +
+                    "<br> Total de Personas Gentrificables: {T_GENT_S}" +
+                    "<br> Total Personas >25 años: {T_PE_25_S}" +
+                    "<br> Total Personas con Educación Superior: {T_PE_ES_S}" +
+                    "<br> Total Personas con Empleo: {T_PE_EM_S}" +
+                    "<br> Total Personas sin Empleo: {T_PE_SE_S}" +
+                    "<br> Total Personas con Empleo Gerencial: {T_PE_EG_S}" +
+                    "<br> Promedio de Personas por Vivienda: {P_PE_V}";
+            }
+        };
+
+        capaBarrios = new FeatureLayerRico({
+            url : "https://services9.arcgis.com/1dyQOpYtlvpIzdDa/arcgis/rest/services/Barrios_CentroHistorico_F/FeatureServer",
+
+        });
+        capaResultados2010 = new FeatureLayerRico({
+            url : "https://services9.arcgis.com/1dyQOpYtlvpIzdDa/arcgis/rest/services/datos_sector2010_f/FeatureServer",
+            outFields: ["T_HO_S","T_PE_S","T_GENT_S","T_PE_25_S","T_PE_ES_S","T_PE_EM_S","T_PE_SE_S","T_PE_EG_S","P_PE_V"],
+            popupTemplate:popUp2,
+            opacity: 0.3
+        });
+        mapa.remove(capaInicial);
+        mapa.add(capaBarrios);
+        mapa.add(capaResultados2010);
+
     }
 }
+
+
+
+
+
