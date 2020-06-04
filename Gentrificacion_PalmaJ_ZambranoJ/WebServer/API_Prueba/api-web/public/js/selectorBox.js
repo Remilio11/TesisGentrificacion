60d4a600dd163a1bc364d4c408194aee073d09e5
jorgez93 <joluzaib@hotmail.com>
2020-06-04 00:44:34 -0500

    graficos terminados

--- Gentrificacion_PalmaJ_ZambranoJ/WebServer/API_Prueba/api-web/public/js/selectorBox.js
+++ Gentrificacion_PalmaJ_ZambranoJ/WebServer/API_Prueba/api-web/public/js/selectorBox.js
@@ -3,8 +3,8 @@ function obtenerValor(){
     var rdb = document.getElementsByName("radio");
     for(var x=0;x<rdb.length;x++){
         if(rdb[x].checked){
-
             cambioCapa(rdb[x].value)
+
         }
     }
 }
