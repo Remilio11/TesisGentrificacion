5c3bb15cbae001a80a7505247504a26f8947dbcb
jorgez93 <joluzaib@hotmail.com>
2020-06-18 18:52:05 -0500

    tesis fin v1.0

--- Gentrificacion_PalmaJ_ZambranoJ/WebServer/API_Prueba/api-web/public/js/selectorBox.js
+++ Gentrificacion_PalmaJ_ZambranoJ/WebServer/API_Prueba/api-web/public/js/selectorBox.js
@@ -14,19 +14,4 @@ function esconderDivs(){
     document.getElementById("content5").style.display = "none";
     document.getElementById("footer1").style.display = "none";
 }
-function obtenerValor1(){
 
-
-    document.getElementById('pdf').style.display = 'none';
-
-
-    var rdb = document.getElementsByName("periodo");
-    for(var x=0;x<rdb.length;x++){
-
-
-        if(rdb[x].checked){
-            cambioCapa(rdb[x].value)
-            console.log('entre obtener')
-        }
-    }
-}
